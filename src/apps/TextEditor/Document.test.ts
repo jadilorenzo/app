b3e344955032190f38d2d184e2c34fd054993b78
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2022-02-17 17:54:09 -0500

    Delete characters

--- src/apps/TextEditor/Document.test.ts
+++ src/apps/TextEditor/Document.test.ts
@@ -1,25 +1,54 @@
-import Document from "./Document"
+import Document from './Document'
 
-describe("Document", () => {
-    test("exists", () => {
+describe('Document', () => {
+    test('exists', () => {
         const doc = new Document()
         expect(doc.allText).toBe('')
     })
 
-    test("handles single keystroke", () => {
-      const doc = new Document();
-      expect(doc.keyStroke('a').allText).toBe('a')
+    test('handles single keystroke', () => {
+        const doc = new Document()
+        expect(doc.keyStroke('a').allText).toBe('a')
     })
 
-    test("handles several keystroke", () => {
-      const doc = new Document();
-      expect(doc.keyStroke("a").keyStroke("b").allText).toBe("ab")
+    test('handles several keystrokes', () => {
+        const doc = new Document()
+        expect(
+            doc
+                .keyStroke('a')
+                .keyStroke('b').allText
+        ).toBe('ab')
     })
 
-    test("handles moving cursor", () => {
-      const doc = new Document();
-      expect(
-        doc.keyStroke("a").keyStroke("b").cursorLeft().cursorLeft().location
-      ).toBe(0)
+    test('handles moving cursor', () => {
+        const doc = new Document()
+        expect(
+            doc
+                .keyStroke('a')
+                .keyStroke('b')
+                .cursorLeft().location
+        ).toBe(1)
+    })
+
+    test('handles inserting with moved cursor', () => {
+        const doc = new Document()
+        expect(
+            doc
+                .keyStroke('c')
+                .cursorLeft()
+                .keyStroke('b')
+                .cursorLeft()
+                .keyStroke('a')
+                .cursorLeft().allText
+        ).toBe('abc')
+    })
+
+    test('handles deleting single characters', () => {
+        const doc = new Document()
+        expect(
+            doc
+                .keyStroke('a')
+                .delete().allText
+        ).toBe('')
     })
 })
--- src/apps/TextEditor/Document.ts
+++ src/apps/TextEditor/Document.ts
@@ -1,35 +1,62 @@
 interface Paragraph {
     type: 'text' | 'image'
     style?: 'bold' | 'italics' | 'underlined' | 'strikethrough' | 'custom' | 'none' | undefined
+    styleCSS?: string
     content: string
     newLine?: boolean
 }
 
-const insert = (arr: any[], index: number, newItem: string) => [
-  // part of the array before the specified index
-  ...arr.slice(0, index),
-  // inserted item
-  newItem,
-  // part of the array after the specified index
-  ...arr.slice(index),
-];
+type Selection = [number, number]
+
+const insert = (arr: unknown[], index: number, newItem: string) => [
+    ...arr.slice(0, index),
+    newItem,
+    ...arr.slice(index),
+]
 
 
 class Document {
-    document: Paragraph[];
-    location: number;
-    allText: string;
+    document: Paragraph[]
+    location: number
+    allText: string
+    selection: Selection | undefined
+    locationInParagraph: number
+    paragraphIndex: number
 
     constructor() {
         this.document = [
             {
-                type: "text",
-                style: "none",
-                content: "",
+                type: 'text',
+                style: 'none',
+                content: '',
             },
-        ];
+        ]
         this.location = 0
-        this.allText = this.document.map((p) => p.content).join("")
+        this.allText = this.document.map((p) => p.content).join('')
+        this.selection = undefined
+        this.paragraphIndex = 0
+        this.locationInParagraph = 0
+       
+    }
+
+    _getLocation() {
+        const locationToParagraphIndex = [0]
+        for (const paragraph of this.document) {
+            paragraph.content.split('').forEach(() => {
+                locationToParagraphIndex.push(this.document.indexOf(paragraph))
+            })
+        }
+        this.paragraphIndex = locationToParagraphIndex[this.location]
+
+        const previousParagraphs = this.document.splice(0, this.paragraphIndex)
+
+        let previousParagraphCharacters = 0
+        for (const paragraph of previousParagraphs) {
+            previousParagraphCharacters =
+            previousParagraphCharacters + paragraph.content.length
+        }
+
+        this.locationInParagraph = this.location - previousParagraphCharacters
     }
 
     cursorLeft(): this {
@@ -47,32 +74,28 @@ class Document {
     }
 
     keyStroke(key: string): this {
-        const locationToParagraphIndex : number[] = [0]
-        for (const paragraph of this.document) {
-            paragraph.content.split('').forEach(() => {
-               locationToParagraphIndex.push(this.document.indexOf(paragraph))
-            })
-        }
-
-        // for every previous paragraph add charcters and then subtract from total location
-        const previousParagraphs = this.document.splice(
-          0,
-          locationToParagraphIndex[this.location]
-        )
-
-        let previousParagraphCharacters = 0
-        for (const paragraph of previousParagraphs) {
-            previousParagraphCharacters = previousParagraphCharacters + paragraph.content.length
-        }
-
-        this.document[locationToParagraphIndex[this.location]].content = 
-            insert(
-                this.document[locationToParagraphIndex[this.location]].content.split(""),
-                this.location-previousParagraphCharacters,
-                key
-            ).join("")
-        this.allText = this.document.map((p) => p.content).join("")
+        this._getLocation()
+        this.document[this.paragraphIndex].content = insert(
+            this.document[this.paragraphIndex].content.split(''),
+            this.locationInParagraph,
+            key
+        ).join('')
+        this.allText = this.document.map((p) => p.content).join('')
         this.location = this.location + 1
+
+        return this
+    }
+    
+    delete(): this {
+        this._getLocation()
+        this.document[this.paragraphIndex].content = this.document[
+            this.paragraphIndex
+        ].content
+            .split('')
+            .splice(this.locationInParagraph, 1)
+            .join('')
+        this.allText = this.document.map((p) => p.content).join('')
+        this.location = this.location - 1
 
         return this
     }
