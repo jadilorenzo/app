11f7c5be582ddc070128e73da89e9d4577c7ae91
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2022-06-07 11:46:46 -0400

    Document remodel

---
+++ src/apps/TextEditor/Doc.test.ts
@@ -0,0 +1,192 @@
+import Document, { CursorLocation, TextElement } from './Doc'
+
+test('moves cursor when typing from blank', () => {
+    const doc = new Document()
+    const text = 'hello world'
+
+    text.split('').forEach(char => doc.keyStroke(char))
+
+    expect(doc.location).toEqual({ x: text.length, y: 0 })
+    expect(doc.text).toEqual('hello world')
+
+    expect(doc.lines).toHaveLength(2)
+    expect(doc.lines[0]).toHaveLength(text.length)
+    expect(doc.lines[1]).toEqual([])
+})
+
+test('moves cursor left', () => {
+    const doc = new Document()
+    const firstLine = 'hello'
+
+    firstLine.split('').forEach((char) => doc.keyStroke(char))
+    doc.cursorLeft()
+
+    expect(doc.location).toEqual({ x: firstLine.length - 1, y: 0 })
+    expect(doc.text).toEqual('hello')
+})
+
+test('deletes and moves cursor back left', () => {
+    const doc = new Document()
+    const firstLine = 'hello'
+
+    firstLine.split('').forEach(char => doc.keyStroke(char))
+    doc.backspace()
+
+    expect(doc.location).toEqual({ x: firstLine.length - 1, y: 0 })
+    expect(doc.text).toEqual('hell')
+})
+
+test('moves cursor with newline', () => {
+    const doc = new Document()
+    const firstLine = 'hello'
+    const secondLine = 'world'
+
+    firstLine.split('').forEach(char => doc.keyStroke(char))
+    doc.newLine()
+    secondLine.split('').forEach(char => doc.keyStroke(char))
+
+    expect(doc.location).toEqual({ x: secondLine.length, y: 1 })
+
+    expect(doc.text).toEqual('hello\nworld')
+
+    expect(doc.lines).toHaveLength(3)
+    expect(doc.lines[0]).toHaveLength(firstLine.length)
+    expect(doc.lines[1]).toHaveLength(secondLine.length)
+    expect(doc.lines[2]).toEqual([])
+})
+
+test('elementIndexAtLocation', () => {
+    const doc = new Document()
+
+    'hello'.split('').forEach(char => doc.keyStroke(char))
+    doc.newLine()
+    'world'.split('').forEach(char => doc.keyStroke(char))
+
+    expect(doc.elementIndexAtLocation({ x: 0, y: 0})).toEqual(0)
+    expect(doc.elementIndexAtLocation({ x: 6, y: 0})).toEqual(6)
+    expect(doc.elementIndexAtLocation({ x: 0, y: 1})).toEqual(7)
+
+    // Invalid location
+    expect(() => doc.elementIndexAtLocation({ x: 7, y: 0})).toThrowError()
+    expect(() => doc.elementIndexAtLocation({ x: 0, y: 2})).toThrowError()
+    expect(() => doc.elementIndexAtLocation({ x: 6, y: 1})).toThrowError()
+})
+
+test('selects text and catches invalid selection', () => {
+    const doc = new Document()
+    const selection: [CursorLocation, CursorLocation] = [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+    ]
+
+    'hello'.split('').forEach((char) => doc.keyStroke(char))
+    doc.select(selection)
+
+    expect(doc.selection).toEqual(selection)
+
+
+    // Invalid location
+    expect(() =>
+        doc.select([
+            { x: 0, y: 0 },
+            { x: 6, y: 0 },
+        ])
+    ).toThrowError()
+    expect(() =>
+        doc.select([
+            { x: 0, y: 0 },
+            { x: 0, y: 1 },
+        ])
+    ).toThrowError()
+})
+
+test('selects text and catches invalid selection', () => {
+    const doc = new Document()
+    const selection: [CursorLocation, CursorLocation] = [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+    ]
+
+    'hello'.split('').forEach((char) => doc.keyStroke(char))
+    doc.select(selection)
+
+    expect(doc.selection).toEqual(selection)
+
+    // Invalid location
+    expect(() =>
+        doc.select([
+            { x: 0, y: 0 },
+            { x: 6, y: 0 },
+        ])
+    ).toThrowError()
+    expect(() =>
+        doc.select([
+            { x: 0, y: 0 },
+            { x: 0, y: 1 },
+        ])
+    ).toThrowError()
+})
+
+test('styles all characters in a selection', () => {
+    const doc = new Document()
+    const selection: [CursorLocation, CursorLocation] = [
+        { x: 0, y: 0 },
+        { x: 2, y: 0 },
+    ]
+
+    'hello'.split('').forEach((char) => doc.keyStroke(char))
+    doc.select(selection)
+    doc.style('BOLD')
+
+    expect((doc.document[0] as TextElement)?.style).toBe('BOLD')
+    expect((doc.document[1] as TextElement)?.style).toBe('BOLD')
+    expect((doc.document[2] as TextElement)?.style).toBe('BOLD')
+})
+
+test('moves selection back', () => {
+    const doc = new Document()
+
+    'hello'.split('').forEach((char) => doc.keyStroke(char))
+    doc.location = {x: 2, y: 0}
+    doc.backspace()
+
+    expect(doc.location).toEqual({ x: 1, y:0})
+    expect(doc.text).toBe('hllo')
+    expect(doc.document.length).toBe(4)
+})
+
+
+test('deletes all characters in a selection', () => {
+    const doc = new Document()
+    const selection: [CursorLocation, CursorLocation] = [
+        { x: 0, y: 0 },
+        { x: 2, y: 0 },
+    ]
+
+    'hello'.split('').forEach((char) => doc.keyStroke(char))
+    doc.select(selection)
+    doc.backspace()
+
+    expect(doc.text).toBe('llo')
+    expect(doc.document.length).toBe(3)
+})
+
+test('deletes selection over newline', () => {
+    const doc = new Document()
+    const selection: [CursorLocation, CursorLocation] = [
+        { x: 4, y: 0 },
+        { x: 2, y: 1 },
+    ]
+
+    // hell|o \n
+    // w|orld
+
+    'hello'.split('').forEach((char) => doc.keyStroke(char))
+    doc.newLine()
+    'world'.split('').forEach((char) => doc.keyStroke(char))
+
+    doc.select(selection)
+    doc.backspace()
+
+    expect(doc.document.length).toBe(7)
+})
---
+++ src/apps/TextEditor/Doc.ts
@@ -0,0 +1,310 @@
+import { v4 as uuidv4 } from 'uuid'
+
+const exampleData = {
+    x: 0,
+    y: 0,
+    activeTextStyle: 'none',
+    activeBlockStyle: 'MATH',
+    document: [
+        {
+            char: 'H',
+            style: 'BOLD',
+        },
+        {
+            char: 'e',
+            style: 'UNDERLINE',
+        },
+        {
+            char: 'l',
+            style: 'STRIKETHROUH',
+        },
+        {
+            char: 'l',
+            style: 'ITALICS',
+        },
+        {
+            char: 'o',
+            style: 'none',
+        },
+        {
+            char: '⇥',
+            style: 'none',
+        },
+        {
+            text: 'x^2',
+            style: 'MATH',
+            block: true,
+        },
+    ],
+}
+
+interface NewlineElement {
+  type: 'newline';
+  id: string;
+}
+
+export interface TextElement {
+  type: 'text';
+  char: string;
+  style?: 'BOLD' | 'UNDERLINE' | 'STRIKETHROUH' | 'ITALICS' | string;
+  id: string;
+}
+
+interface BlockElement {
+  type: 'block';
+  text: string;
+  block: boolean;
+  style?: 'MATH' | string;
+  id: string;
+}
+
+type Element = NewlineElement | TextElement | BlockElement;
+
+export interface CursorLocation {
+  x: number;
+  y: number;
+}
+
+const remove = (arr: unknown[], index: number) => [
+    ...arr.slice(0, index),
+    ...arr.slice(index + 1),
+]
+
+class Document {
+    location: CursorLocation
+    activeTextStyle:
+    | 'BOLD'
+    | 'UNDERLINE'
+    | 'STRIKETHROUH'
+    | 'ITALICS'
+    | 'none'
+    | string
+    activeBlockStyle: 'MATH' | 'none' | string
+    document: Element[]
+    selection: [CursorLocation, CursorLocation] | undefined
+
+    constructor() {
+        this.location = { x: 0, y: 0 }
+        this.activeTextStyle = 'none'
+        this.activeBlockStyle = 'none'
+        this.document = [] // exampleData.document
+        this.selection = undefined
+    }
+
+    get text(): string {
+        return this.document
+            .map((elem) => {
+                switch (elem.type) {
+                case 'block':
+                    return elem.text
+                case 'text':
+                    return elem.char
+                case 'newline':
+                    return '\n'
+                }
+            })
+            .join('')
+    }
+
+    get lines(): Element[][] {
+        const lines: Element[][] = []
+        let currentLine: Element[] = []
+
+        for (const element of this.document) {
+            if (element.type === 'newline') {
+                lines.push(currentLine)
+                currentLine = []
+            } else {
+                currentLine.push(element)
+            }
+        }
+
+        lines.push(currentLine)
+
+        // Always end with empty new line
+        if (currentLine.length > 0) {
+            lines.push([])
+        }
+
+        return lines
+    }
+
+    _setDocument(document: Element[]) {
+        this.document = document
+    }
+
+    keyStroke(key: string): this {
+        if (key.length !== 1) {
+            throw new Error(`Single key expected. Received "${key}"`)
+        }
+
+        this._setDocument([
+            ...this.document,
+            { type: 'text', char: key, id: uuidv4() },
+        ])
+        this.cursorRight()
+        return this
+    }
+
+    _updateLocation(deltaX = 0): this {
+        const lines = this.lines
+
+        const { x, y } = this.location
+
+        let newX = x,
+            newY = y
+        let deltaXLeft = Math.abs(deltaX)
+        let lineRemainder
+
+        if (deltaX > 0) {
+            lineRemainder = lines[y].length - x
+            while (deltaXLeft > 0) {
+                if (deltaXLeft <= lineRemainder) {
+                    newX += deltaXLeft
+                    deltaXLeft = 0
+                } else {
+                    newX = 0
+                    newY += 1
+                    deltaXLeft -= lineRemainder + 1
+                    lineRemainder = lines[newY].length
+                }
+            }
+        } else if (deltaX < 0) {
+            lineRemainder = x
+            while (deltaXLeft > 0) {
+                if (deltaXLeft <= x) {
+                    newX -= deltaXLeft
+                    deltaXLeft = 0
+                } else {
+                    newY -= 1
+                    newX = lines[newY].length 
+                    deltaXLeft -= lineRemainder + 1
+                    lineRemainder = lines[newY].length
+                }
+            }
+        }
+
+        // console.log('_updateLocation', { deltaX, x: newX, y: newY })
+        this.location = { x: newX, y: newY }
+
+        return this
+    }
+
+    cursorLeft(): this {
+        this._updateLocation(-1)
+        return this
+    }
+
+    cursorRight(): this {
+        this._updateLocation(1)
+        return this
+    }
+
+    newLine(): this {
+        this._setDocument([...this.document, { type: 'newline', id: uuidv4() }])
+        this.cursorRight()
+        return this
+    }
+
+    elementIndexAtLocation(location: CursorLocation): number {
+        let index = 0
+        let x = 0,
+            y = 0
+
+        while (y < location.y) {
+            if (this.document[index].type === 'newline') {
+                y += 1
+                index += 1
+            }
+            index += 1
+        }
+
+        while (x < location.x) {
+            if (x > 0 && this.document[index - 1].type === 'newline') {
+                throw new Error(`Invalid cursor location: ${JSON.stringify(location)}`)
+            }
+
+            x += 1
+            index += 1
+        }
+
+        return index
+    }
+
+    select(selection: [CursorLocation, CursorLocation]): this {
+        if (
+            Math.abs(
+                this.elementIndexAtLocation(selection[0]) -
+          this.elementIndexAtLocation(selection[1])
+            ) > this.document.length
+        )
+            throw new Error(`Invalid selection: ${JSON.stringify(selection)}`)
+
+        this.selection = selection
+        return this
+    }
+
+    backspace(): this {
+        if (this.selection === undefined) {
+            this.document = remove(
+                this.document,
+                this.elementIndexAtLocation(this.location) - 1
+            ) as Element[]
+            this.cursorLeft()
+            
+        } else {
+            const selection = this.selection
+            this.location = {...selection[1], 'x': selection[1].x - selection[1].y } // calculate for the newline character
+            this.selection = undefined
+            while (
+                this.location.x !== selection[0].x  ||
+                this.location.y !== selection[0].y
+            ) {
+                // console.log(this.text)
+                // console.log(selection, this.location)
+                this.backspace()
+            }
+            const documentSelection = this.document.slice(
+                this.elementIndexAtLocation(selection[0]),
+                this.elementIndexAtLocation(selection[1])
+            )
+
+            // remove new-lines in deleted selection
+            if (
+                documentSelection
+                    .map((element) => element.type === 'newline')
+                    .includes(true)
+            ) {
+                const newLineSelectionIndex = documentSelection.map(
+                    (element) => element.type === 'newline'
+                ).indexOf(true)
+
+                const newLineSelectionIndexId = documentSelection[newLineSelectionIndex].id
+
+                const newLineIndex = this.document
+                    .map((element) => element.id === newLineSelectionIndexId)
+                    .indexOf(true)
+                
+                this.document = remove(this.document, newLineIndex) as Element[]
+            }
+        }
+        return this
+    }
+
+    style(style: 'BOLD' | 'UNDERLINE' | 'STRIKETHROUH' | 'ITALICS' | string): this {
+        if (this.selection) {
+            const startIndex = this.elementIndexAtLocation(this.selection[0])
+            const endIndex = this.elementIndexAtLocation(this.selection[1])
+            this.document = this.document.map((textElement, index) => {
+                if (index >= startIndex && index <= endIndex) {
+                    return {...textElement, style}
+                }
+            }) as Element[]
+        } else {
+            throw new Error('No selection to style.')
+        }
+        return this
+    } 
+}
+
+export default Document
